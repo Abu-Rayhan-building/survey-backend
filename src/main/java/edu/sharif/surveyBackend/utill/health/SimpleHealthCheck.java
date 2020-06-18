e82e89ab6402e49c51b65a2f7c52568bbcf7157e
hosseinmp76 <hosseinmp76@viratech.ir>
2020-06-18 11:11:09 +0430

    health & mail

---
+++ src/main/java/edu/sharif/surveyBackend/utill/health/SimpleHealthCheck.java
@@ -0,0 +1,16 @@
+package edu.sharif.surveyBackend.utill.health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Liveness
+@ApplicationScoped
+public class SimpleHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("my Simple health check");
+    }
+}
